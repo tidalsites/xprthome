050e6405073925a03887062c3e02304ee22bced5
Devin Haynes <tidalsites@gmail.com>
2022-08-09 16:21:47 -0400

    Added vite-plugin-pwa to register service worker for PWA

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.scss";
-import * as serviceWorkerRegistration from "../serviceWorkerRegistration";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
